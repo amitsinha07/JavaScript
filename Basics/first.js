2b9c1a58a170ac9b0b8e2f3dd69cb746212f45cf
Amit Sinha <aamitsinha456@gmail.com>
2021-02-26 01:32:55 +0530

    Changes Added

--- Basics/first.js
+++ Basics/first.js
@@ -1,1 +1,189 @@
-console.log("Hello World");
+// 1. Ways to print in JavaScript
+
+/*console.log("Hello World");
+alert("Its me");
+document.write("This is my Document.")*/
+
+
+// 2. JavaScript Console API
+
+/*console.log("Hello World!!");
+console.warn("This is a warning");
+console.error("This an error");*/
+
+
+// 3. JavaScript Variables
+
+/*var num1 = 34;
+var num2 = 26;
+console.log(num1 + num2);*/
+
+// 4. Data types in JavaScript
+
+// Numbers
+
+/*var num1 = 455;
+var num2 = 56.7;*/
+
+// Strings
+// var str1 = "My name is Amit";
+
+// Object 
+
+/*var Student = {
+    Name: "Amit",
+    age: 20,
+    Gender: "Male"
+}
+
+console.log(Student)*/
+
+// Booleans
+
+/*var a = true;
+var b = false;
+console.log(b);*/
+
+// var und = undefined;
+// console.log(und);
+
+
+/* At a very high level, there are two types of data types in JavaScript
+1. Primitive
+2. Non Primitive */
+
+// var arr = [1, 2, "rahul", 4, 5];
+// console.log(arr);
+
+
+//////////////////////////////////////////
+//////// Operators in JavaScript /////////
+//////////////////////////////////////////
+
+// Arithmetic Operators : +, -, *, /, %
+// Assignment Operators : =, -=, +=, /=
+// Comparision Operators: ==, <=, >=, !=, >, <
+// Logical operators: &&, ||, !
+
+
+
+/////////////////////////////////////////
+///////// Function in JavaScript ////////
+/////////////////////////////////////////
+
+/*function avg(a, b){
+    c = (a + b)/2;
+    return c;
+}
+
+c1 = avg(4, 6);
+c2 = avg(14, 16);
+console.log(c1, c2);*/
+
+
+
+///////////////////////////////////////////
+///////// Conditionals in JavaScript //////
+///////////////////////////////////////////
+
+// var age = prompt("Enter you age: ");
+// if(age < 18){
+//     console.log("You are a kid");
+// }else if(age >= 18 && age < 40) {
+//     console.log("You are young");
+// } else {
+//     console.log("You are old");
+// }
+
+
+
+////////////////////////////////////////////
+////////// Loops in JavaScript /////////////
+////////////////////////////////////////////
+
+/////////////// For Loop //////////////
+// var arr = [1,2,3,4,5,6];
+// // console.log(arr);
+//  for(var i=0; i < arr.length; i++){
+//      if(arr[i] == 2){
+//          //break;
+//          continue;
+//      }
+//      console.log(arr[i]);
+// }
+
+
+///////// ForEach Loop ///////////////
+
+// arr.forEach(function(element){
+//     console.log(element);
+// })
+
+
+/////////// While Loop ///////////////
+
+// i = 0;
+// while(i<arr.length){
+//     console.log(arr[i]);
+//     i++;
+// }
+
+/////////// Do While Loop ////////////
+
+// var i = 0;
+// do{
+//     console.log(arr[i]);
+//     i++;
+// } while(i<arr.length)
+
+
+
+
+///////////////////////////////////////////
+//////////////// Array Methods ////////////
+///////////////////////////////////////////
+
+// let myArr = ["Fan", "Camera", 34, null, true];
+
+// console.log(myArr.length);
+// myArr.pop();
+// myArr.push("Amit");
+// myArr.shift();
+// myArr.unshift("Amit");
+// arr = myArr.toString();
+
+// console.log(myArr.sort());
+
+
+
+///////////////////////////////////////////
+/////// String Methods in Javascript //////
+///////////////////////////////////////////
+
+// let str = "Abhay is a good boy";
+// // console.log(str.length);
+// // console.log(str.indexOf("good"));
+// // console.log(str.lastIndexOf("a"));
+
+// console.log(str.slice(0,5));
+// arr = str.replace("Abhay","Rahul");
+// console.log(arr);
+
+
+
+
+////////////////////////////////////////////
+////////// Date in JavaScript //////////////
+////////////////////////////////////////////
+
+// let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.getTime());
+// console.log(myDate.getFullYear());
+// console.log(myDate.getDay());
+// console.log(myDate.getMinutes());
+
+
+
+
+
