bf9939b20b7ec7f7685e3497b2e9b03e76ef1a3b
Amit Sinha <aamitsinha456@gmail.com>
2021-02-27 21:24:17 +0530

    JavaScript basics updated

--- Basics/first.js
+++ Basics/first.js
@@ -240,3 +240,44 @@ console.log(c1, c2);*/
 // console.log(distinctCount([1,2,1,2,1]));
 
 
+
+/////////////////////////////////////////////
+////// Map Data Structure in JavaScript /////
+/////////////////////////////////////////////
+
+const myMap = new Map();
+const key1 = 'myStr', key2 = {}, key3 = function(){};
+myMap.set(key1, 'This is a string');
+myMap.set(key2, 'This a blank obj');
+myMap.set(key3, 'This is an empty function' );
+console.log(myMap.size);
+
+// let value1 = myMap.get(key3);
+// console.log(value1);
+
+// for(let[key, value] of myMap){
+//     console.log(value);
+// }
+
+// myMap.forEach((value, key)=>{
+//     console.log('Key is ',key);
+//     console.log('Value is ', value);
+// })
+
+
+
+//Converting map to an array 
+
+let myArray = Array.from(myMap);
+console.log('Map to array is ',myArray);
+
+let myKeysArray = Array.from(myMap.keys());
+console.log('Map to keys array is ',myKeysArray);
+
+let myValuesArray = Array.from(myMap.values());
+console.log('Map to values array is ', myValuesArray);
+
+
+
+
+
