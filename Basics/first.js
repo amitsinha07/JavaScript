d52d48828f3d1d249dc4ac3b05808ea9f593fe17
Amit Sinha <aamitsinha456@gmail.com>
2021-03-14 12:04:45 +0530

    JavaScript Promises

--- Basics/first.js
+++ Basics/first.js
@@ -282,5 +282,24 @@ console.log(c1, c2);*/
 /////////////////// JavaScript Promises /////////////////////
 /////////////////////////////////////////////////////////////
 
+function func1() {
+    return new Promise(function (resolve, reject){
+        setTimeout(()=>{
+            const error = true;
+            if(error){
+                console.log("Function: Your promise has been resolved");
+                resolve();
+            } else {
+                console.log('Function: Your promise has not been resolved');
+                reject('Sorry not fulfilled');
+            }
+        },2000);
+    })
+}
 
+func1().then(function(){
+    console.log("Amit: Thanks for resolving");
+}).catch(function(error){
+    console.log("Amit: Very bad bro. Reason: " + error);
+})
 
